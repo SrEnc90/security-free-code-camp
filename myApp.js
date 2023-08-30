88da9e7559e9b1a6a568b9c67be1a4558136aa01
SrEnc90 <carl.enc90@gmail.com>
2023-08-30 10:13:58 -0500

    ready to deploy v2

--- myApp.js
+++ myApp.js
@@ -2,56 +2,6 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
-app.listen(PORT, () => {
-  console.log(`🥦 Useful Programmer Info Security App started on port ${PORT}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
@@ -62,5 +12,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`🥦 Useful Programmer Info Security App started on port ${port}`);
 });
